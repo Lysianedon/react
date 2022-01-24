264a6684aa3f84309dd30b783c40b87f588be5c2
lysiandon <don.lysiane@gmail.com>
2022-01-24 22:43:16 +0100

    added all boxes

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -3,8 +3,9 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./DefaultValues";
 import DefaultValues from "./DefaultValues";
-import Steps from "./components/Steps";
+import Box from "./components/box";
 import "./styles/global.css";
+// import "/Users/lysianedon/Documents/KONEXIO/react/chapitre-01/elsy/src/styles/global.css";
 
 const tempMin = -20;
 const tempMax = 40;
@@ -20,8 +21,17 @@ class App extends React.Component {
       <div className="container-fluid">
         <div className="row">
           <DefaultValues heartMin={heartMin} stepsMin={stepsMin} tempMin={tempMin} ></DefaultValues>
-          <Steps></Steps>
-          
+          {/* Water Box */}
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L"></Box>
+
+          {/* Steps Box */}
+          <Box icon="directions_walk" color="black" value={3000} unit="steps"></Box>
+
+          {/* Heart Box */}
+          <Box icon="favorite" color="red" value={120} unit="bpm"></Box>
+
+          {/* Temperature Box */}
+          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C"></Box>
         </div>
       </div>
     );
